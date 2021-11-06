4a26bc1bf105d0aa30d2770d2c02ddff0c40fcd2
srz_zumix <zumix.cpp@gmail.com>
2021-11-06 16:13:38 +0900

    fix

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# shellcheck disable=SC2086
+
 cd "${GITHUB_WORKSPACE}" || exit
 
 TEMP_PATH="$(mktemp -d)"
@@ -12,14 +14,14 @@ echo '::endgroup::'
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 echo '::group:: Checking infer report with reviewdog 🐶 ...'
-cat "${INPUT_REPORT_PATH}/report.txt" \
-  | reviewdog -efm="%E%f:%l: %trror: %m,%E%f:%l: %tarning: %m,%C\ \ %n.\ %.%#,%C%m,%-G%.%#" \
+reviewdog -efm="%E%f:%l: %trror: %m,%E%f:%l: %tarning: %m,%C\ \ %n.\ %.%#,%C%m,%-G%.%#" \
     -name="${INPUT_TOOL_NAME}" \
     -reporter="${INPUT_REPORTER}" \
     -filter-mode="${INPUT_FILTER_MODE}" \
     -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
     -level="${INPUT_LEVEL}" \
-    ${INPUT_REVIEWDOG_FLAGS}
+    ${INPUT_REVIEWDOG_FLAGS} \
+    < "${INPUT_REPORT_PATH}/report.txt"
 EXIT_CODE=$?
 echo '::endgroup::'
 
